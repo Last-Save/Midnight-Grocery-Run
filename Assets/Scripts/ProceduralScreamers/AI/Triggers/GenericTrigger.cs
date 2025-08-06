df8d7c07c29778b43064aecbf1cc7305f53d60d9
vvtelnov <vvtelnov@mail.ru>
2025-08-06 20:46:17 +0500

    feat: Стартовая стена

---
+++ Assets/Scripts/ProceduralScreamers/AI/Triggers/GenericTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace ProceduralScreamers.AI.Triggers
+{
+    public enum TriggerType
+    {
+        AllType
+        
+    }
+    
+    public class GenericTrigger : MonoBehaviour
+    {
+        [SerializeField] private TriggerType _triggerType;
+        [SerializeField] private bool _canBeTriggeredAgain;
+
+        private uint _id;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
