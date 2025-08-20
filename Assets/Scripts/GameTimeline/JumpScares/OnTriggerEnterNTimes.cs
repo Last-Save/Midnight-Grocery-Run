2c161bca3bccae9cedab79df9ac05df467a9e726
vvtelnov <vvtelnov@mail.ru>
2025-08-20 14:13:04 +0500

    Сделал скримеров в первой зоне

---
+++ Assets/Scripts/GameTimeline/JumpScares/OnTriggerEnterNTimes.cs
@@ -0,0 +1,26 @@
+﻿using UHFPS.Runtime;
+using UnityEngine;
+
+namespace GameTimeline.JumpScares
+{
+    public class OnTriggerEnterNTimes : MonoBehaviour
+    {
+        [SerializeField] JumpscareTrigger _jumpScare;
+        [SerializeField] int _timesToEnterToEnableJumpscare;
+        
+        private int _currentEnteredTimes = 0;
+        private readonly string _playerTag = "Player";
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag(_playerTag)) return;
+
+            _currentEnteredTimes++;
+
+            if (_currentEnteredTimes >= _timesToEnterToEnableJumpscare)
+            {
+                _jumpScare.TriggerJumpscare();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameTimeline/JumpScares/RadioJumpscare.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using GameTimeline.Quests;
+using UHFPS.Runtime;
+using UnityEngine;
+
+namespace GameTimeline.JumpScares
+{
+    public class RadioJumpscare : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _radioSoundOutput;
+        [SerializeField] private GameObject _radio;
+        
+        private readonly string _playerTag = "Player";
+        
+        private bool _hasTriggered = false;
+
+        public void TurnRadioOn()
+        {
+            if (!_hasTriggered) return;
+            if (_radioSoundOutput == null || _radioSoundOutput.isPlaying) return;
+
+            ObjectsToggler.SetLayerToInteract(_radio);
+            _radioSoundOutput.Play();
+        }
+
+        // Метод вызывается в GemeObject radio в инспекторе
+        public void TurnRadioOff()
+        {
+            if (_radioSoundOutput != null && _radioSoundOutput.isPlaying)
+            {
+                _radioSoundOutput.Stop();
+            }
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (!_hasTriggered && other.CompareTag(_playerTag))
+            {
+                _hasTriggered = true;
+
+                TurnRadioOn();
+                
+                Destroy(gameObject);
+            }
+        }
+        
+        
+    }
+}
